3fc9ecb5a4639379beed8c10d9b2ae2d16aa929f
cady2015 <604548568@qq.com>
2019-07-06 22:09:28 +0800

    1.斐波那契数列 2.汉诺塔问题

---
+++ part0-lettecode-algorithms/lettecode/src/main/java/QA_TwoSum/Solution_2_lookupTable.java
@@ -0,0 +1,32 @@
+package QA_TwoSum;
+
+import java.util.HashMap;
+
+/**
+ * 解决方法2：使用查找表--通过空间换时间
+ *
+ * 思路：
+ *  1.开启一个循环，从数组头部遍历至尾部
+ *  2.通过一张HashMap保存中间结果，只保存当前数及其下标
+ *  3.判断条件：如果能够从该HashMap中找到当前值与target的差值==keySet中的任意一个值时，将其作为结果进行返回
+ * Created by llj on 2019/7/5.
+ */
+public class Solution_2_lookupTable {
+    public int[] twoSum(int[] nums, int target) {
+        //使用查找表
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            //在已经建立的查找表中寻找是否有满足的元素
+            if (map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
+            }
+            //使用HashMap中的put方法，如果key已经存在newValue则会覆盖原来的oldValue
+            map.put(nums[i], i);
+
+        }
+        return res;
+    }
+}
